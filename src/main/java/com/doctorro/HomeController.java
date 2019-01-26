122cbdd654818a4cd4d729e456e5c79d2bd437df
heedaelee <adguy72@gmail.com>
2019-01-26 21:02:47 +0900

    1/26 백업

---
+++ src/main/java/com/doctorro/HomeController.java
@@ -0,0 +1,14 @@
+package com.doctorro;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	@RequestMapping("/index.htm")
+	public String home() {
+		System.out.println("탄다");
+		return "index.html";
+	}
+	
+}
